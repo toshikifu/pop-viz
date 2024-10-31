b2120a8d6132d92a02668decc54b55cb285d362c
Toshiki <toshiki.fujii@outlook.com>
2024-10-31 18:13:47 +0900

    👽️ write fetcher

--- app/api/resasApi.ts
+++ app/api/resasApi.ts
@@ -1,5 +1,10 @@
 // src/api/resasApi.ts
-import type { Prefecture, ResasResponse } from "~/types/resas";
+import type {
+	FetchPopulationOptions,
+	PopulationCompositionResponse,
+	Prefecture,
+	ResasResponse,
+} from "~/types/resas";
 import axiosClient from "~/utils/axiosClient";
 
 export const fetchPrefectures = async (): Promise<Prefecture[]> => {
@@ -9,6 +14,29 @@ export const fetchPrefectures = async (): Promise<Prefecture[]> => {
 		return response.data.result;
 	} catch (error) {
 		console.error("Error fetching prefectures: ", error);
+		throw error;
+	}
+};
+
+export const fetchPopulationComposition = async ({
+	prefCode,
+	addArea,
+}: FetchPopulationOptions): Promise<PopulationCompositionResponse> => {
+	try {
+		const params: Record<string, unknown> = { prefCode };
+
+		if (addArea) {
+			params.addArea = addArea; // カンマ区切りのaddAreaパラメータを追加
+		}
+
+		const response = await axiosClient.get<
+			ResasResponse<PopulationCompositionResponse>
+		>("/population/composition/perYear", {
+			params,
+		});
+		return response.data.result;
+	} catch (error) {
+		console.error("Error fetching population composition: ", error);
 		throw error;
 	}
 };
