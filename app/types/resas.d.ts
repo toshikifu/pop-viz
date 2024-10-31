bfa52d1304ec66f2833b5755ce6c3a0b19169c20
Toshiki <toshiki.fujii@outlook.com>
2024-10-31 18:13:23 +0900

    🏷️ define population composition response type

--- app/types/resas.d.ts
+++ app/types/resas.d.ts
@@ -8,3 +8,28 @@ export interface ResasResponse<T> {
 	message: string | null;
 	result: T;
 }
+
+export interface PopulationData {
+	year: number;
+	value: number;
+	rate?: number;
+}
+
+export interface PopulationComposition {
+	label: string;
+	data: PopulationData[];
+}
+
+export interface PopulationCompositionResponse {
+	boundaryYear: number;
+	data: PopulationComposition[];
+}
+
+export interface ResasResponse<T> {
+	message: string | null;
+	result: T;
+}
+export interface FetchPopulationOptions {
+	prefCode: number;
+	addArea?: string; // "1_,13_" や "1_01100,13_13101" などの形式で複数エリアを指定
+}
