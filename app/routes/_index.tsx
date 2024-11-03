92d7c0445b2a5531a0c63028083c2046a43e1dea
Toshiki <toshiki.fujii@outlook.com>
2024-11-03 16:10:11 +0900

    ✨ connect to population composition API

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,69 @@
-import { useLoaderData } from "@remix-run/react";
-import { fetchPrefectures } from "~/api/resasApi";
-import type { Prefecture } from "~/types/resas";
+import {
+	type ClientLoaderFunctionArgs,
+	Form,
+	useLoaderData,
+	useSearchParams,
+} from "@remix-run/react";
+import { fetchPopulationComposition, fetchPrefectures } from "~/api/resasApi";
 
-export const clientLoader = async (): Promise<Prefecture[]> => {
+export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
 	const prefectures = await fetchPrefectures();
-	return prefectures;
+
+	const searchParams = new URLSearchParams(request.url.split("?")[1]);
+
+	const prefCodes = searchParams.getAll("prefCode");
+
+	const prefCode = prefCodes.shift();
+
+	const addArea = prefCodes.length
+		? prefCodes.map((code) => `${code}_`).join(",")
+		: undefined;
+
+	if (prefCode) {
+		const populationData = await fetchPopulationComposition({
+			prefCode,
+			addArea,
+		});
+		return { prefectures, populationData };
+	}
+	return { prefectures };
 };
 
 export default function Index() {
-	const prefectures = useLoaderData<typeof clientLoader>();
+	const { prefectures, populationData } = useLoaderData<typeof clientLoader>();
+	const [searchParams] = useSearchParams();
 	return (
-		<div>
-			{prefectures.map((prefecture) => (
-				<div key={prefecture.prefCode}>{prefecture.prefName}</div>
-			))}
-		</div>
+		<>
+			<Form method="get">
+				{prefectures.map((prefecture) => (
+					<label key={prefecture.prefCode}>
+						<input
+							type="checkbox"
+							name="prefCode"
+							value={prefecture.prefCode}
+							defaultChecked={searchParams
+								.getAll("prefCode")
+								.includes(prefecture.prefCode)}
+						/>
+						{prefecture.prefName}
+					</label>
+				))}
+				<button type="submit">Submit</button>
+			</Form>
+			<div>
+				{populationData?.data.map((data) => (
+					<div key={data.label}>
+						<h2>{data.label}</h2>
+						{data.data.map((item) => (
+							<div key={item.year}>
+								<h3>{item.year}</h3>
+								<p>{item.value}</p>
+								<p>{item.rate}</p>
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+		</>
 	);
 }
