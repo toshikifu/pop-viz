baedb782c109371558793a8a4f2e8025161463d8
Toshiki <toshiki.fujii@outlook.com>
2024-11-05 18:28:31 +0900

    ✨ apply components to view

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,9 @@ import {
 	useSearchParams,
 } from "@remix-run/react";
 import { fetchPopulationComposition, fetchPrefectures } from "~/api/resasApi";
+import Button from "~/src/ui/Button";
+import Card from "~/src/ui/Card";
+import Checkbox from "~/src/ui/Checkbox";
 
 export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
 	const prefectures = await fetchPrefectures();
@@ -35,22 +38,36 @@ export default function Index() {
 	return (
 		<>
 			<Form method="get">
-				{prefectures.map((prefecture) => (
-					<label key={prefecture.prefCode}>
-						<input
-							type="checkbox"
-							name="prefCode"
-							value={prefecture.prefCode}
-							defaultChecked={searchParams
-								.getAll("prefCode")
-								.includes(prefecture.prefCode.toString())}
-						/>
-						{prefecture.prefName}
-					</label>
-				))}
-				<button type="submit">Submit</button>
+				<div className="flex items-center gap-2">
+					<h2 className="text-4xl">都道府県</h2>
+					<Button className="w-20 h-12" type="submit">
+						決定
+					</Button>
+				</div>
+
+				<div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-10 gap-2 mt-4">
+					{prefectures.map((prefecture) => (
+						<label
+							key={prefecture.prefCode}
+							htmlFor={prefecture.prefCode.toString()}
+							className="flex items-center gap-2"
+						>
+							<Checkbox
+								name="prefCode"
+								id={prefecture.prefCode}
+								value={prefecture.prefCode}
+								defaultChecked={searchParams
+									.getAll("prefCode")
+									.includes(prefecture.prefCode.toString())}
+							/>
+							<p>{prefecture.prefName}</p>
+						</label>
+					))}
+				</div>
 			</Form>
-			<div>
+
+			<h2 className="mt-6 text-4xl">人口推移グラフ</h2>
+			<Card className="p-3">
 				{populationData?.data.map((data) => (
 					<div key={data.label}>
 						<h2>{data.label}</h2>
@@ -63,7 +80,7 @@ export default function Index() {
 						))}
 					</div>
 				))}
-			</div>
+			</Card>
 		</>
 	);
 }
